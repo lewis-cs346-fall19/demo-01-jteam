18391e9c0e5053ee16f69d919dde1769eb095d83
FirstName <name@email.arizona.edu>
2019-09-03 10:38:30 -0700

    added try-except block

--- net_client.py
+++ net_client.py
@@ -4,8 +4,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ("localhost", 40836)
 sock.connect(addr)
 for i in range(100):
-	msg = i * " "
-	sock.sendall(msg.encode())
-	reply = sock.recv(1024)
-	print(reply)
+	try:
+		msg = i * " "
+		sock.sendall(msg.encode())
+		reply = sock.recv(1024)
+		print(reply)
+	except:
+		sock.close()
 sock.close()
