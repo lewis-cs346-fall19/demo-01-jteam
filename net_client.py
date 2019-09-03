7f5b668184a82eabaaa59639b9083d306a89b2fb
FirstName <name@email.arizona.edu>
2019-09-03 10:53:52 -0700

    connects multiple times

--- net_client.py
+++ net_client.py
@@ -2,15 +2,13 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ("localhost", 40836)
-sock.connect(addr)
 print("got here")
 for i in range(100):
+	sock.connect(addr)
 	print("got there")
 	try:
 		msg = i * " "
 		sock.sendall(msg.encode())
 		reply = sock.recv(1024)
 		print(reply)
-	except:
-		sock.close()
 sock.close()
