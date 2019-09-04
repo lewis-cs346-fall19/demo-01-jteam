d122768b9ac7fe9c8ce09de71d66dd5a13e76f5a
FirstName <name@email.arizona.edu>
2019-09-04 14:24:30 -0700

    Connects only once

--- net_client.py
+++ net_client.py
@@ -3,13 +3,13 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ("localhost", 40836)
 print("got here")
+sock.connect(addr)
 for i in range(100):
-	sock.connect(addr)
 	print("got 1")
 	msg = i * "O"
 	sock.sendall(msg.encode())
 	print("got 2")
 	reply = sock.recv(1024).decode()
 	print(reply)
-	sock.close()
-	print("got 3")
+sock.close()
+print("got 3")
