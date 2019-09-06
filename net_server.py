e81cffafa20bb81305ce5ee2be8fad15dce67ad8
FirstName <name@email.arizona.edu>
2019-09-05 21:12:41 -0700

    More connects

--- net_server.py
+++ net_server.py
@@ -4,12 +4,12 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ("localhost", 40836)
 sock.bind(addr)
 sock.listen(5)
-(connectedSock, clientAddress) = sock.accept()
 while True:
+	(connectedSock, clientAddress) = sock.accept()
 	print("got 1")
 	try:
 		msg = connectedSock.recv(1024).decode()
-		print("got 2: " + msg)
+		print("got 2: " + msg + type(msg)
 		output = msg.length() * "-"
 		connectedSock.sendall(output.encode())
 		print("got 3")
